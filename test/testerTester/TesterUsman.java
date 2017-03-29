efdf8403a0dafb73d4bb7d93af33240d432669b1
ali-0 <ali-0@MSI>
2017-03-29 10:04:58 +0200

    Created println voor Usman Fag

---
+++ test/testerTester/TesterUsman.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testerTester;
+
+import org.junit.*;
+
+/**
+ *
+ * @author ali-0
+ */
+public class TesterUsman {
+    
+
+    public TesterUsman() {
+        
+        System.out.println("Usman is een faggggggg");
+    }
+    
+}
